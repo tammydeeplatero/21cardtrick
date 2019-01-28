743f11772c4ff71e8cab80ec06af8bda13f56f55
Tammy Platero <tammy.platero@gmail.com>
2019-01-28 07:46:42 -0700

    Revert "Revert "Added classes from charts""
    This reverts commit 0f7ac0dcdc6fdd5347993ddd9df941e6f3816836.

---
+++ 21Card/21Card/Dealer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21Card
+{
+    class Dealer
+    {
+        //Attributes
+        //dealNumber
+    }
+
+    //methods
+    //deal
+    //revealCard
+    //pickupCards
+}
