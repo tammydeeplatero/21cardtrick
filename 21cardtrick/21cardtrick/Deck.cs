7ffcb110932ed93a6af67232cd7477f59ee5ce58
cody.alves <cody.alves24@gmail.com>
2019-01-28 09:19:20 -0700

    added the classes

---
+++ 21cardtrick/21cardtrick/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21cardtrick
+{
+    class Deck
+    {
+        private List<Card> _deck;
+
+        private void GenerateDeck()
+        {
+            _deck = new List<Card>();
+            for (int i = 0; i <= 3; i++)
+            for (int j = 1; j <= 13; j++)
+                _deck.Add(new Card((Card.Suit)i, j));
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            _deck = _deck.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+
+        public void Deal()
+        {
+
+        }
+    }
+}
