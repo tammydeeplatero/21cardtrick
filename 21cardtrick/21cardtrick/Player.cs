c837ff74dda1f4077dcc0e9c7274b30daf6a8b44
Tyler Davies <tyspace01@gmail.com>
2019-01-28 09:52:56 -0700

    added player class

--- 21cardtrick/21cardtrick/Player.cs
+++ 21cardtrick/21cardtrick/Player.cs
@@ -8,5 +8,40 @@ namespace _21cardtrick
 {
     class Player
     {
+        private bool hasSelectedCard = false;
+        public Player()
+        {
+
+        }
+
+
+        public int indicateColumn(int column1, int column2, int column3)
+        {
+            int choice = 0;
+            //string choice = "";
+            if (column1 == 1)
+            {
+                choice = 1;
+                //choice = "1 was selected";
+            }
+            if (column2 == 1)
+            {
+                choice = 2;
+                //choice = "2 was selected";
+            }
+            if (column3 == 1)
+            {
+                choice = 3;
+                //choice = "3 was selected";
+            }
+            return choice;
+        }
+
+        public bool pickCard()
+        {
+
+            hasSelectedCard = true;
+            return hasSelectedCard;
+        }
     }
 }
