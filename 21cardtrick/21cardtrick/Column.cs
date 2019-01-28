17fcf942f05fce4ea2ccdd89497e267774d56173
Tyler Davies <tyspace01@gmail.com>
2019-01-28 10:59:56 -0700

    Adding board and column

---
+++ 21cardtrick/21cardtrick/Column.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21cardtrick
+{
+    public class Column
+    {
+        //attributes id
+    }
+    
+    //method addCard(card)
+}
